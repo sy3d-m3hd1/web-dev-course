643b7f0f2d4d1bc04d565cdc447f3c3325e6df81
sy3d-m3hd1 <tatheermehdi85@gmail.com>
2025-11-17 16:31:42 +1100

    project

---
+++ react.components/React-components/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold mb-6">  This is the recipe</h1>
+
+      <p className="text-xl mb-4">Count: {count}</p>
+
+      <button
+        onClick={() => setCount(count + 1)}
+        className="px-6 py-3 bg-blue-600 hover:bg-blue-700 rounded-xl text-lg transition-all"
+      >
+        Increase
+      </button>
+    </div>
+  );
+}
---
+++ react.components/React-components/src/Student.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class Student extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <div><p>Im student class component</p></div>
+        );
+    }
+}
+ 
+export default Student;
